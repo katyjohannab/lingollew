897e4490af14eae0b95991aa84181a42335b776d
katyjohannab <124362373+katyjohannab@users.noreply.github.com>
2024-07-23 11:26:32 +0100

    Update index.js

--- js/index.js
+++ js/index.js
@@ -30,4 +30,38 @@ document.addEventListener('DOMContentLoaded', () => {
         });
         document.getElementById('toggleLanguage').innerText = isCymraeg ? 'English' : 'Cymraeg';
     }
+
+    // Amazon Polly Text-to-Speech Function
+    function synthesizeSpeech(text, language = 'welsh') {
+        AWS.config.region = 'eu-west-2';
+        AWS.config.credentials = new AWS.Credentials('AKIAVRUVVGBB7E2WU6EL', 'HSCO8NnICJaU03F3ztyFiQhBPz8lDxkTMGx++a2N');
+        var polly = new AWS.Polly();
+        var params = {
+            OutputFormat: 'mp3',
+            Text: text,
+            VoiceId: language === 'welsh' ? 'Gwyneth' : 'Geraint',
+            TextType: 'text'
+        };
+        polly.synthesizeSpeech(params, function(err, data) {
+            if (err) {
+                console.log(err, err.stack);
+            } else {
+                var uInt8Array = new Uint8Array(data.AudioStream);
+                var arrayBuffer = uInt8Array.buffer;
+                var blob = new Blob([arrayBuffer]);
+                var url = URL.createObjectURL(blob);
+                var audio = new Audio(url);
+                audio.play();
+            }
+        });
+    }
+
+    // Example of attaching TTS function to buttons
+    document.querySelectorAll('.tts-button').forEach(button => {
+        button.addEventListener('click', () => {
+            const textElement = document.querySelector(button.getAttribute('data-target'));
+            synthesizeSpeech(textElement ? textElement.innerText : '');
+        });
+    });
 });
+
